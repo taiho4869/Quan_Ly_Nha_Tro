c55b17a5d31cefee973b6d9ede3f0c72c2bb146f
Ho Van Tai <taihopz123456@gmail.com>
2026-09-23 15:08:29 +0700

    Them

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quan_Ly_Nha_Tro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
